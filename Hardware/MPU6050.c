f871791e4e69377ecf99a4476cc373d6aed4f3d4
YE <15599805164@163.com>
2026-08-20 22:22:58 +0800

    feat: archive MPU6050 sensor integration

---
+++ Hardware/MPU6050.c
@@ -0,0 +1,93 @@
+#include "stm32f10x.h"                  // Device header
+#include "MyI2C.h"
+#include "MPU6050_Reg.h"
+
+#define MPU6050_ADDRESS		0xD0
+
+void MPU6050_WriteReg(uint8_t RegAddress, uint8_t Data)
+{
+	MyI2C_Start();
+	MyI2C_SendByte(MPU6050_ADDRESS);
+	MyI2C_ReceiveAck();
+	MyI2C_SendByte(RegAddress);
+	MyI2C_ReceiveAck();
+	MyI2C_SendByte(Data);
+	MyI2C_ReceiveAck();
+	MyI2C_Stop();
+}
+
+uint8_t MPU6050_ReadReg(uint8_t RegAddress)
+{
+	uint8_t Data;
+	
+	MyI2C_Start();
+	MyI2C_SendByte(MPU6050_ADDRESS);
+	MyI2C_ReceiveAck();
+	MyI2C_SendByte(RegAddress);
+	MyI2C_ReceiveAck();
+	
+	MyI2C_Start();
+	MyI2C_SendByte(MPU6050_ADDRESS | 0x01);
+	MyI2C_ReceiveAck();
+	Data = MyI2C_ReceiveByte();
+	MyI2C_SendAck(1);
+	MyI2C_Stop();
+	
+	return Data;
+}
+void MPU6050_ReadRegs(uint8_t RegAddress, uint8_t *DataArray, uint8_t Count)
+{
+	uint8_t i;
+	
+	MyI2C_Start();
+	MyI2C_SendByte(MPU6050_ADDRESS);
+	MyI2C_ReceiveAck();
+	MyI2C_SendByte(RegAddress);
+	MyI2C_ReceiveAck();
+	
+	MyI2C_Start();
+	MyI2C_SendByte(MPU6050_ADDRESS | 0x01);
+	MyI2C_ReceiveAck();
+	for (i = 0; i < Count; i ++)
+	{
+		DataArray[i] = MyI2C_ReceiveByte();
+		if (i < Count - 1)
+		{
+			MyI2C_SendAck(0);
+		}
+		else
+		{
+			MyI2C_SendAck(1);
+		}
+	}
+	MyI2C_Stop();
+	
+}
+void MPU6050_Init(void)
+{
+	MyI2C_Init();
+	MPU6050_WriteReg(MPU6050_PWR_MGMT_1, 0x01);
+	MPU6050_WriteReg(MPU6050_PWR_MGMT_2, 0x00);
+	MPU6050_WriteReg(MPU6050_SMPLRT_DIV, 0x09);
+	MPU6050_WriteReg(MPU6050_CONFIG, 0x06);
+	MPU6050_WriteReg(MPU6050_GYRO_CONFIG, 0x18);
+	MPU6050_WriteReg(MPU6050_ACCEL_CONFIG, 0x18);
+}
+
+uint8_t MPU6050_GetID(void)
+{
+	return MPU6050_ReadReg(MPU6050_WHO_AM_I);
+}
+
+void MPU6050_GetData(int16_t *AX, int16_t *AY, int16_t *AZ, int16_t *GX, int16_t *GY, int16_t *GZ)
+{
+	uint8_t Data[14];
+	MPU6050_ReadRegs(MPU6050_ACCEL_XOUT_H, Data, 14);
+	*AX = (Data[0] << 8) | Data[1];
+	*AY = (Data[2] << 8) | Data[3];
+	*AZ = (Data[4] << 8) | Data[5];
+	*GX = (Data[8] << 8) | Data[9];
+	*GY = (Data[10] << 8) | Data[11];
+	*GZ = (Data[12] << 8) | Data[13];
+
+}
--- User/main.c
+++ User/main.c
@@ -5,30 +5,81 @@
 #include "OLED.h"
 #include "LED.h"
 #include "Delay.h"
+#include "Key.h"
+#include "MPU6050.h"
 
-uint16_t count;
+
+int16_t AX, AY, AZ, GX, GY, GZ;
+uint16_t TimeErrorFlag;
 
 int main(void)
 {
+    OLED_Init();
+    MPU6050_Init();
     Timer_Init();
 
-    OLED_Init();
+    while(1)
+    {
+
+        OLED_Printf(0, 0, OLED_8X16, "%+06d", AX);
+        OLED_Printf(0, 16, OLED_8X16, "%+06d", AY);
+        OLED_Printf(0, 32, OLED_8X16, "%+06d", AZ);
+        OLED_Printf(64, 0, OLED_8X16, "%+06d", GX);
+        OLED_Printf(64, 16, OLED_8X16, "%+06d", GY);
+        OLED_Printf(64, 32, OLED_8X16, "%+06d", GZ);
+        OLED_Printf(0, 48, OLED_8X16, "TimeErrorFlag: %d", TimeErrorFlag);
+        OLED_Update();  
+     }
+
+}
+
+ void TIM1_UP_IRQHandler(void)
+ {
+     if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
+     {
+        TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+
+        MPU6050_GetData(&AX, &AY, &AZ, &GX, &GY, &GZ);
+
+        if(TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
+        {
+            TimeErrorFlag = 1;
+            TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+        }
+     }
+ }
+
+
+
+// uint16_t count;
+// uint8_t KeyNum;
+
+// int main(void)
+// {
+//     Timer_Init();
+//     OLED_Init();
+
+//     Key_Init();
 
     
-    while (1)
-    {
-        OLED_Printf(0, 0, OLED_8X16, "Counter: %05d", count);
-        OLED_Update();
+//     while (1)
+//     {
+//         KeyNum = Key_GetNum();
+//         if (KeyNum)
+//         {
+//             OLED_Printf(0, 2, OLED_8X16, "Key: %d", KeyNum);
+//             OLED_Update();
+//         }
+//     }
+// }
 
-    }
+// void TIM1_UP_IRQHandler(void)
+// {
+//     if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
+//     {
+//         count++;
 
-}
-
-void TIM1_UP_IRQHandler(void)
-{
-    if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
-    {
-        count++;
-        TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
-    }
-}
+//         Key_Tick();
+//         TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+//     }
+// }
